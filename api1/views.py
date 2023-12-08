1af41a786148e6db6334453f42fe16c1bf543ca9
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-12-08 21:11:55 +0500

    imports

--- api1/views.py
+++ api1/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.http import HttpRequest,JsonResponse,FileResponse
+from rest_framework.parsers import MultiPartParser, FormParser
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.authentication import TokenAuthentication, BasicAuthentication
 
-# Create your views here.
+
+
+class Login(APIView):
+    authentication_classes = [BasicAuthentication]
+    def post(self, request):
+        user = request.user
+        token = Token.objects.get_or_create(user=user)
+        token = str(token[0])
+        return Response({'detail':True,'token':token},status=status.HTTP_201_CREATED)
+
