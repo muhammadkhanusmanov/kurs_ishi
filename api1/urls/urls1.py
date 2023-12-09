41222e7bde016c03ae87aa37d6795f6cbdbec213
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-12-09 19:04:31 +0500

    url

--- api1/urls/urls1.py
+++ api1/urls/urls1.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from ..views import LoginView
+from ..views import LoginView, AdminView,GetData
 
 urlpatterns = [
-    path('login/', LoginView.as_view())
+    path('login/', LoginView.as_view()),
+    path('add/category/', AdminView.as_view()),
+    path('get/category/<str:id>', GetData.as_view()),
 ]
