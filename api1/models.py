e5ca804e7da8b997e9a00ff6f4cda7f50723bfbb
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-12-09 19:58:30 +0500

    model mahsulot

--- api1/models.py
+++ api1/models.py
@@ -7,3 +7,12 @@ class Category(models.Model):
     def __str__(self) -> str:
         return self.name
 
+class Mahsulot(models.Model):
+    name = models.CharField(max_length=75)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    price = models.CharField(max_length=45)
+    description = models.TextField()
+    img = models.ImageField(upload_to='mahsulot')
+
+    def __str__(self) -> str:
+        return self.name
