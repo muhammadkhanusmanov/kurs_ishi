604433aae95e8bc2157a82971c337d3a92ae8ea9
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-12-09 19:58:39 +0500

    .

--- api1/serialzers.py
+++ api1/serialzers.py
@@ -1,8 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import Category
+from .models import Category,Mahsulot
 
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
+        fields = '__all__'
+
+
+class MahsulotSerializer(ModelSerializer):
+    class Meta:
+        model = Mahsulot
         fields = '__all__'
