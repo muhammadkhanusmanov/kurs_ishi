53f179888d37b9686ee9aeeaae3bcce77c34a461
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-12-09 19:57:46 +0500

    .

--- api1/urls/urls1.py
+++ api1/urls/urls1.py
@@ -4,6 +4,7 @@ from ..views import LoginView, AdminView,GetData,CategoryView
 urlpatterns = [
     path('login/', LoginView.as_view()),
     path('add/category/', AdminView.as_view()),
+    path('add/product/', AdminView.as_view()),
     path('get/category/<str:id>', GetData.as_view()),
     path('allcategory/',CategoryView.as_view())
 ]
