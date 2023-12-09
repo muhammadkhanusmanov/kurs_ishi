016d0a943d414e017e832a6bdffa5e3d1cb6c816
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-12-09 12:54:47 +0500

    category model

--- api1/models.py
+++ api1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=75)
+    img = models.ImageField(upload_to='category')
+
+    def __str__(self) -> str:
+        return self.name
+
